7aa1c8504fe6c24b2ab0d521735bdf839898e507
BackRunner <liaoyp126@126.com>
2019-01-06 01:40:08 +0800

    basic feature

--- WebpConverter/MainWindow.xaml.cs
+++ WebpConverter/MainWindow.xaml.cs
@@ -11,18 +11,54 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using System.IO;
+using MahApps.Metro.Controls.Dialogs;
 
 namespace WebpConverter
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private string filePath;
+
+        private async void Rect_dragArea_Drop(object sender, DragEventArgs e)
+        {
+            string path = ((Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+            string extension = Path.GetExtension(path);
+            if (extension.Equals(".jpg") || extension.Equals(".jpeg") || extension.Equals(".png"))
+            {
+                filePath = path;
+                tb_fileName.Text = filePath;
+                lbl_outputFormat.Visibility = Visibility.Hidden;
+                cb_outputFormat.Visibility = Visibility.Hidden;
+            } else if (extension.Equals(".webp"))
+            {
+                filePath = path;
+                tb_fileName.Text = filePath;
+                lbl_outputFormat.Visibility = Visibility.Visible;
+                cb_outputFormat.Visibility = Visibility.Visible;
+            } else
+            {
+                await this.ShowMessageAsync("类型错误", "工具只支持jpg，png，webp格式的文件。");
+            }
+        }
+
+        private void Lbl_dragArea_Drop(object sender, DragEventArgs e)
+        {
+            Rect_dragArea_Drop(sender, e);
+        }
+
+        private void Btn_selectFile_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
