00219832ff90f43e5a131c9157019b8ae68b767f
BackRunner <liaoyp126@126.com>
2019-01-06 20:03:58 +0800

    basic functions completed

--- WebpConverter/MainWindow.xaml.cs
+++ WebpConverter/MainWindow.xaml.cs
@@ -17,6 +17,9 @@ using MahApps.Metro.Controls.Dialogs;
 using Microsoft.Win32;
 using IniParser;
 using IniParser.Model;
+using ImageProcessor.Imaging.Formats;
+using ImageProcessor.Plugins.WebP.Imaging.Formats;
+using ImageProcessor;
 
 namespace WebpConverter
 {
@@ -25,12 +28,6 @@ namespace WebpConverter
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-            configPath = appPath + @"config.ini";
-        }
-
         #region == 声明 ==
         //path
         private string filePath;
@@ -41,7 +38,25 @@ namespace WebpConverter
         private FileIniDataParser iniParser = new FileIniDataParser();
         private IniData config = null;
 
+        //dialog
+        private MetroDialogSettings generalDialogSetting = new MetroDialogSettings()
+        {
+            AffirmativeButtonText = "确定",
+            DialogTitleFontSize = 18,
+            DialogMessageFontSize = 16
+        };
+
+        //flag
+        private bool isToWebP = false;
+
         #endregion
+
+        #region == 窗体初始化 ==
+        public MainWindow()
+        {
+            InitializeComponent();
+            configPath = appPath + @"config.ini";
+        }
 
         private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
         {
@@ -59,8 +74,13 @@ namespace WebpConverter
                 if (config != null)
                 {
                     //根据设置项更新UI
-                    cb_autoConvert.IsChecked = bool.Parse(config["Config"]["AutoConvert"]);
-                    cb_outputFormat.SelectedIndex = int.Parse(config["Config"]["OutputSelect"]);
+                    bool isAutoConvert = false;
+                    bool.TryParse(config["Config"]["AutoConvert"], out isAutoConvert);
+                    cb_autoConvert.IsChecked = isAutoConvert;
+
+                    int formatIndex = 0;
+                    int.TryParse(config["Config"]["OutputSelect"],out formatIndex);
+                    cb_outputFormat.SelectedIndex = formatIndex;
                 }
             }
             else
@@ -68,16 +88,107 @@ namespace WebpConverter
                 config = new IniData();
             }
         }
+        #endregion
 
-        private void Btn_convert_Click(object sender, RoutedEventArgs e)
+        #region == 格式转换 ==
+
+        private async void Btn_convert_Click(object sender, RoutedEventArgs e)
         {
-
+            if (filePath == null || filePath.Length <= 0)
+            {
+                await this.ShowMessageAsync("错误", "请选择需要转换的图片。", MessageDialogStyle.Affirmative, generalDialogSetting);
+            } else
+            {
+                if (isToWebP)
+                {
+                    ConvertToWebP(filePath);
+                } else
+                {
+                    switch (cb_outputFormat.SelectedIndex)
+                    {
+                        case 0:
+                            ConvertToPNG(filePath);
+                            break;
+                        case 1:
+                            ConvertToJPGHQ(filePath);
+                            break;
+                        case 2:
+                            ConvertToJPGNQ(filePath);
+                            break;
+                    }
+                }
+            }
         }
 
-        private void ConvertToWebP()
+        private void ConvertToWebP(string path)
         {
-
+            ISupportedImageFormat webpFormat = new WebPFormat { Quality=100 };
+            ConvertImage(path, webpFormat);
         }
+
+        private void ConvertToJPGHQ(string path)
+        {
+            ISupportedImageFormat jpgFormat = new JpegFormat { Quality = 100 };
+            ConvertImage(path, jpgFormat);
+        }
+
+        private void ConvertToJPGNQ(string path)
+        {
+            ISupportedImageFormat jpgFormat = new JpegFormat { Quality = 80 };
+            ConvertImage(path, jpgFormat);
+        }
+
+        private void ConvertToPNG(string path)
+        {
+            ISupportedImageFormat pngFormat = new PngFormat { Quality = 100 };
+            ConvertImage(path, pngFormat);
+        }
+
+        private async void ConvertImage(string path, ISupportedImageFormat format)
+        {
+            byte[] b_image;
+            try
+            {
+                b_image = File.ReadAllBytes(path);
+            }
+            catch (Exception e)
+            {
+                await this.ShowMessageAsync("错误", "读入图片时发生错误: " + e.Message, MessageDialogStyle.Affirmative, generalDialogSetting);
+                return;
+            }
+            using (MemoryStream inStream = new MemoryStream(b_image))
+            {
+                using (ImageFactory imageFactory = new ImageFactory(preserveExifData: true))
+                {
+                    string savePath = Path.GetDirectoryName(path) + @"/" + Path.GetFileNameWithoutExtension(path) + ".webp";
+                    if (File.Exists(savePath))
+                    {
+                        var existDialogRes = await this.ShowMessageAsync("文件已存在", "将要保存的文件: " + savePath + " 已存在，是否覆盖？", MessageDialogStyle.AffirmativeAndNegative, new MetroDialogSettings()
+                        {
+                            AffirmativeButtonText = "覆盖",
+                            NegativeButtonText = "取消",
+                            DialogTitleFontSize = 16
+                        });
+                        if (!(existDialogRes == MessageDialogResult.Affirmative))
+                        {
+                            return;
+                        }
+                    }
+                    try
+                    {
+                        imageFactory.Load(inStream).Format(format).Save(savePath);
+                    }
+                    catch (Exception e)
+                    {
+                        await this.ShowMessageAsync("错误", "转换时发生错误: " + e.Message, MessageDialogStyle.Affirmative, generalDialogSetting);
+                        return;
+                    }
+                    await this.ShowMessageAsync("转换完成", "图片已转换完成。", MessageDialogStyle.Affirmative, generalDialogSetting);
+                }
+            }
+        }
+
+        #endregion
 
         #region == 文件拖放与选择 ==
 
@@ -94,9 +205,11 @@ namespace WebpConverter
 
         private void Btn_selectFile_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog fileDialog = new OpenFileDialog();
-            fileDialog.Title = "请选择需要转换的图片文件";
-            fileDialog.Filter = "图片文件|*.jpg|图片文件|*.png|图片文件|*.jpeg|图片文件|*.webp|所有文件|*.*";
+            OpenFileDialog fileDialog = new OpenFileDialog
+            {
+                Title = "请选择需要转换的图片文件",
+                Filter = "图片文件|*.jpg|图片文件|*.png|图片文件|*.jpeg|图片文件|*.webp|所有文件|*.*"
+            };
             if (fileDialog.ShowDialog() == true)
             {
                 string path = fileDialog.FileName;
@@ -111,6 +224,7 @@ namespace WebpConverter
             {
                 filePath = path;
                 tb_fileName.Text = filePath;
+                isToWebP = true;
                 lbl_outputFormat.Visibility = Visibility.Hidden;
                 cb_outputFormat.Visibility = Visibility.Hidden;
             }
@@ -118,12 +232,13 @@ namespace WebpConverter
             {
                 filePath = path;
                 tb_fileName.Text = filePath;
+                isToWebP = false;
                 lbl_outputFormat.Visibility = Visibility.Visible;
                 cb_outputFormat.Visibility = Visibility.Visible;
             }
             else
             {
-                await this.ShowMessageAsync("类型错误", "工具只支持jpg，png，webp格式的文件。");
+                await this.ShowMessageAsync("类型错误", "工具只支持 jpg，png，webp 格式的文件。", MessageDialogStyle.Affirmative, generalDialogSetting);
             }
         }
 
@@ -133,14 +248,22 @@ namespace WebpConverter
 
         private void Cb_autoConvert_Checked(object sender, RoutedEventArgs e)
         {
+            if (config == null)
+            {
+                config = new IniData();
+            }
             config["Config"]["AutoConvert"] = cb_autoConvert.IsChecked.ToString();
             SaveConfig();
         }        
 
         private void Cb_outputFormat_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            if (config == null)
+            {
+                config = new IniData();
+            }
             config["Config"]["AutoConvert"] = cb_outputFormat.SelectedIndex.ToString();
-            SaveConfig();
+            SaveConfig();            
         }
 
         private void SaveConfig()
